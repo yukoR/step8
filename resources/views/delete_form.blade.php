37fafff36b48380cfb77da7a47bea334ddbb348c
yukoR <153795772+yukoR@users.noreply.github.com>
2024-03-28 21:17:41 +0900

    不用なファイルの削除

--- resources/views/delete_form.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- 消去フォーム -->
-<form action="{{ route('product.destroy', $product) }}" method="post">
-    @csrf
-    @method('delete')
-    <button type="submit" class="btn btn-danger btn-sm text-nowrap" id="btnDeleteButton" onclick="return confirm('本当に削除しますか')">削除</button>
-</form>
